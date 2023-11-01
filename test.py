c773e5ccc1e9a0a23dd24bb837e71ecb5e7b4c0d
omrchk <merdeevamir@gmail.com>
2023-11-01 20:41:09 +0400

    всё

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
 
+def funkzia():
+    secret.setText('Секрет!')
+
+app = QApplication([])
+okno = QWidget()
+okno.setWindowTitle('Моё второе приложение')
+okno.move(500, 100)
+okno.resize(1000, 1000)
+knopka = QPushButton('Кнопка')
+line = QVBoxLayout()
+line.addWidget(knopka, alignment = Qt.AlignCenter)
+secret = QLabel('')
+line.addWidget(secret, alignment = Qt.AlignCenter)
+knopka.clicked.connect(funkzia)
+
+okno.setLayout(line)
+okno.show()
+app.exec_()
