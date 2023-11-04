1fab7cf2504639039998a641f6962720a6d16176
omrchk <merdeevamir@gmail.com>
2023-11-04 17:17:25 +0400

    bububub

---
+++ my_app.py
@@ -0,0 +1,33 @@
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from second_win import *
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        hello = QLabel(txt_hello)
+        instruction = QLabel(txt_instruction)
+        self.b_next = QPushButton(txt_next)
+        vline1 = QVBoxLayout()
+        vline1.addWidget(hello, alignment = Qt.AlignLeft)
+        vline1.addWidget(instruction, alignment = Qt.AlignLeft)
+        vline1.addWidget(self.b_next, alignment = Qt.AlignCenter)
+        self.setLayout(vline1)
+    def connects(self):
+        self.b_next.clicked.connect(self.funkzia)
+    def funkzia(self):
+        self.hide()
+        Win2 = SecWin()
+    
+app = QApplication([])
+okno = MainWin()
+app.exec_()
---
+++ second_win.py
@@ -0,0 +1,7 @@
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+
+class SecWin(QWidget):
+    def __init__(self):
+        pass
